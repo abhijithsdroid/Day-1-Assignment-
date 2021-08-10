8df4fc53ed8f13d22ef775a8c589b8a5e9c77059
abhijithsdroid <81155013+abhijithsdroid@users.noreply.github.com>
2021-08-10 12:38:56 +0530

    Add files via upload

---
+++ day1/Student/src/Student.java
@@ -0,0 +1,18 @@
+ class Student {
+		 int roll;
+		 String name;
+		 float marks;
+		 void input (int r,String n,float m) {
+		 	roll=r;
+		 	name=n;
+		 	marks=m;
+		 }
+		 void display()
+		 {
+		 	System.out.println("Name :"+ name );
+		 	System.out.println("Roll No :"+ roll );
+		 	System.out.println("Marks :"+ marks );
+		 }
+		 }
+
+		
---
+++ day1/Student/src/StudentTest.java
@@ -0,0 +1,12 @@
+
+public class StudentTest {
+	public static void main(String[] args) {
+		     	Student ob =new Student();
+		     	
+		     	ob.input(01,"Abhijith",92.54f);
+		     	ob.display();
+		     }
+
+	}
+
+
